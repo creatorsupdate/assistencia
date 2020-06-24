fa2c760a42a8791d8e3fa01c96392a9cca250fd4
creatorsupdate <suporte.cel@outlook.com>
2020-06-24 00:04:02 +0000

    versao2306

---
+++ database.js
@@ -0,0 +1,23 @@
+const Pool = require('pg').Pool;
+
+const pool = new Pool({  
+    user: 'rsxlswlemfntrc', 
+    password: 'fb0d25ed369df4906704bf84fee5aaeb72d3bc7c8b8468eef5e3f2db1467c9d2',
+    host: 'ec2-52-20-248-222.compute-1.amazonaws.com',
+    database: 'db241e1gfgp83',
+    port: '5432',
+    ssl: { rejectUnauthorized: false }
+});
+
+     const sql_insert = `
+        INSERT INTO assistencia (cliente, servico, orcamento, situacao)
+            VALUES
+                ('Xykó', 'Virus corintiano no celular', 'nao tem preço', 'concluído' )
+    `;
+
+    pool.query(sql_insert, function(error, result) {
+        if(error)
+            throw error;
+        
+        console.log(result.rowCount);
+    })
--- server.js
+++ server.js
@@ -1,20 +1,26 @@
 const express = require('express');
+const Pool = require('pg').Pool;
+const cors = require('cors');
 
 const server = express();
+server.use(cors());
+
+const pool = new Pool({  
+    user: 'rsxlswlemfntrc', 
+    password: 'fb0d25ed369df4906704bf84fee5aaeb72d3bc7c8b8468eef5e3f2db1467c9d2',
+    host: 'ec2-52-20-248-222.compute-1.amazonaws.com',
+    database: 'db241e1gfgp83',
+    port: '5432',
+    ssl: { rejectUnauthorized: false }
+});
 
 server.use(express.json());
 
-const assistencia = [
-
-    {cliente: 'Liu Kang', servico: 'Troca de bateria Iphone 7', orcamento: 'R$160,00'},
-    {cliente: 'Nosferatu', servico: 'Troca de tela Samsung A50', orcamento: 'R$680,00'},
-    {cliente: 'Tirocerto karabina', servico: 'Troca de conector de carga, flex do ID e tela j5 Prime', orcamento: 'R$200,00'}
-    
-]
-server.get('/assistencia', function(request, response) {
-    response.json(assistencia);
+server.get('/assistencia', async function(request, response) {
+    const result = await pool.query('SELECT * FROM assistencia');
+    return response.json(result.rows);
 })
-server.post('/assistencia', function(request, response) {
+server.post('/assistencia', async function(request, response) {
     
     //const id = request.body.id;
     //const cliente = request.body.cliente;
@@ -22,38 +28,52 @@ server.post('/assistencia', function(request, response) {
     //const orcamento = request.body.orcamento;
 
     const {cliente, servico, orcamento} = request.body;
+    
+    const sql = `
+    INSERT INTO assistencia (cliente, servico, orcamento, situacao) VALUES ($1, $2, $3, $4, $5)
+    `;
 
-    assistencia.push({cliente, servico, orcamento});
-    response.status(204).send();
-})
+    await pool.query(sql, [cliente, servico, orcamento, situacao]);
+    return response.status(201).send();
+});
 
-server.put('/assistencia/:id', function(request, response) {
-    const id = request.params.id;
-    const {cliente, servico, orcamento} = request.body;
+    //assistencia.push({cliente, servico, orcamento});
+    //response.status(204).send();
 
-    for(let i = 0; i < assistencia.length; i++) {
-        if(assistencia[i].cliente == id) {
-            assistencia[i].cliente = cliente;
-            assistencia[i].servico = servico;
-            assistencia[i].orcamento = orcamento
-            break;
-        };
+server.put('/assistencia/:id', async (request, response) => {
+    const {id} = request.params;
+    const {cliente, servico, orcamento, situacao} = request.body;
+    
+    //for(let i = 0; i < assistencia.length; i++) {
+    //    if(assistencia[i].cliente == id) {
+    //        assistencia[i].cliente = cliente;
+    //        assistencia[i].servico = servico;
+    //        assistencia[i].orcamento = orcamento
+    //        break;
+    //    };
         
-    }
-    return response.status(204).send();
-})
-
-server.delete('/assistencia/:id', function(request, response) {
+    const sql = `
+    UPDATE assistencia SET cliente = $1, servico = $2, orcamento = $3, situacao = $4  WHERE id = $6
+    `;
     
-    const id = request.params.id;
-    
-    for(let i = 0; i < assistencia.length; i++) {
-        if(assistencia[i].cliente == id) {
-            assistencia.splice(i, 1);
-            break;
-        }
-    }
+    await pool.query(sql, [nome,numero,idade,salario,contrato,id]);
     return response.status(204).send();
-})
+});      
+    
+server.delete('/assistencia/:id', async function(req, res){
+    const id = req.params.id;
+    
+    //for(let i = 0; i < assistencia.length; i++) {
+    //    if(assistencia[i].cliente == id) {
+    //        assistencia.splice(i, 1);
+    //        break;
+    //    }
+    //}
+    sql = 'DELETE FROM assistencia WHERE id = $1';
 
+    await pool.query(sql, [id]);
+
+    res.send();
+})
+    
 server.listen(process.env.PORT || 3000);
